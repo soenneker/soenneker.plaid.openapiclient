2e17a52a495062ecf993457864861acb498ed9e6
Jake Soenneker <jake@soenneker.com>
2026-03-31 04:31:24 +0000

    Automated update

--- src/Soenneker.Plaid.OpenApiClient/Models/GetRecipientResponse.cs
+++ src/Soenneker.Plaid.OpenApiClient/Models/GetRecipientResponse.cs
@@ -8,58 +8,17 @@ using System;
 namespace Soenneker.Plaid.OpenApiClient.Models
 {
     /// <summary>
-    /// Recipient metadata fields that are defined by FDX
+    /// GetRecipientResponse defines the response schema for `/fdx/recipient/{recipientId}`
     /// </summary>
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
-    public partial class GetRecipientResponse : IAdditionalDataHolder, IParsable
+    public partial class GetRecipientResponse : global::Soenneker.Plaid.OpenApiClient.Models.FDXRecipientMetadata, IParsable
     {
-        /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
-        public IDictionary<string, object> AdditionalData { get; set; }
-        /// <summary>The recipient name displayed by the Data Provider during the consent flow</summary>
-#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
-#nullable enable
-        public string? ClientName { get; set; }
-#nullable restore
-#else
-        public string ClientName { get; set; }
-#endif
-        /// <summary>Data Recipient Logo URL location</summary>
-#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
-#nullable enable
-        public string? LogoUri { get; set; }
-#nullable restore
-#else
-        public string LogoUri { get; set; }
-#endif
-        /// <summary>The recipient identifier</summary>
-#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
-#nullable enable
-        public string? RecipientId { get; set; }
-#nullable restore
-#else
-        public string RecipientId { get; set; }
-#endif
-        /// <summary>The legal name of the recipient</summary>
-#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
-#nullable enable
-        public string? ThirdPartyLegalName { get; set; }
-#nullable restore
-#else
-        public string ThirdPartyLegalName { get; set; }
-#endif
-        /// <summary>
-        /// Instantiates a new <see cref="global::Soenneker.Plaid.OpenApiClient.Models.GetRecipientResponse"/> and sets the default values.
-        /// </summary>
-        public GetRecipientResponse()
-        {
-            AdditionalData = new Dictionary<string, object>();
-        }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
         /// <returns>A <see cref="global::Soenneker.Plaid.OpenApiClient.Models.GetRecipientResponse"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static global::Soenneker.Plaid.OpenApiClient.Models.GetRecipientResponse CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static new global::Soenneker.Plaid.OpenApiClient.Models.GetRecipientResponse CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
             return new global::Soenneker.Plaid.OpenApiClient.Models.GetRecipientResponse();
@@ -68,28 +27,20 @@ namespace Soenneker.Plaid.OpenApiClient.Models
         /// The deserialization information for the current model
         /// </summary>
         /// <returns>A IDictionary&lt;string, Action&lt;IParseNode&gt;&gt;</returns>
-        public virtual IDictionary<string, Action<IParseNode>> GetFieldDeserializers()
+        public override IDictionary<string, Action<IParseNode>> GetFieldDeserializers()
         {
-            return new Dictionary<string, Action<IParseNode>>
+            return new Dictionary<string, Action<IParseNode>>(base.GetFieldDeserializers())
             {
-                { "client_name", n => { ClientName = n.GetStringValue(); } },
-                { "logo_uri", n => { LogoUri = n.GetStringValue(); } },
-                { "recipient_id", n => { RecipientId = n.GetStringValue(); } },
-                { "third_party_legal_name", n => { ThirdPartyLegalName = n.GetStringValue(); } },
             };
         }
         /// <summary>
         /// Serializes information the current object
         /// </summary>
         /// <param name="writer">Serialization writer to use to serialize this model</param>
-        public virtual void Serialize(ISerializationWriter writer)
+        public override void Serialize(ISerializationWriter writer)
         {
             if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
-            writer.WriteStringValue("client_name", ClientName);
-            writer.WriteStringValue("logo_uri", LogoUri);
-            writer.WriteStringValue("recipient_id", RecipientId);
-            writer.WriteStringValue("third_party_legal_name", ThirdPartyLegalName);
-            writer.WriteAdditionalData(AdditionalData);
+            base.Serialize(writer);
         }
     }
 }
